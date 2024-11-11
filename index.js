2b67f43e901ea5a50c1fcb19d59554f86a8eec83
Елена Павлючук <elenapavlucuk@MacBook-Air-Elena.local>
2024-11-12 02:23:20 +0300

    remove console log

--- index.js
+++ index.js
@@ -4,40 +4,37 @@ function formatName(text) {
     const formattedName = firstLtter + otherLetters;
 
     return formattedName;
-}
+};
+
+const nameInputElement = document.getElementById("name-input");
+const lastNameInputElement = document.getElementById("last-name-input");
+const emailInputElement = document.getElementById("email-input");
+const passwordInputElement = document.getElementById("password-input");
 
 function handleChangeName(event) {
     const name = event.target.value;
-    const nameInputElement = document.getElementById("name-input");
     nameInputElement.setAttribute("value", name);
 };
 
 function handleChangeLastName(event) {
     const lastName = event.target.value;
-    const lastNameInputElement = document.getElementById("last-name-input");
     lastNameInputElement.setAttribute("value", lastName);
 };
 
 function handleChangeEmail(event) {
     const email = event.target.value;
-    const emailInputElement = document.getElementById("email-input");
     emailInputElement.setAttribute("value", email);
-}
+};
 
 function handleChangePassword(event) {
     const password = event.target.value;
-    const passwordInputElement = document.getElementById("password-input");
     passwordInputElement.setAttribute("value", password);
-}
+};
 
 const loginUser = () => {
-    const nameInputElement = document.getElementById("name-input");
-    console.log("name input element: ", nameInputElement.value);
-    const lastNameInputElement = document.getElementById("last-name-input");
-    console.log("last name input element: ", lastNameInputElement.value);
-    const emailInputElement = document.getElementById("email-input");
-    console.log("email input element", emailInputElement.value);
-    const passwordInputElement = document.getElementById("password-input");
-    console.log("password input element", passwordInputElement.value);
-    alert(`Registred: ${formatName(nameInputElement.value)}, ${formatName(lastNameInputElement.value)}, ${emailInputElement.value}!`);
+    nameInputElement.value;
+    lastNameInputElement.value;
+    emailInputElement.value;
+    passwordInputElement.value;
+    alert(`Registred: ${formatName(nameInputElement.value)}, ${formatName(lastNameInputElement.value)}, ${emailInputElement.value}, ${passwordInputElement.value}!`);
 };
